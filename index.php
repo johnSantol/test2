d0308f0f3183c38b2b1c4b7fd70b8a0e3c316849
john <93307570+johnSantol@users.noreply.github.com>
2021-11-13 07:27:26 +0800

    Add files via upload

---
+++ index.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>this is a sample upload on github</title>
+</head>
+<body>
+	<h1>This is a smple upload on PHP</h1>
+</body>
+</html>
